5ff414da1f99d58e484c91d047eae1de8e5676d4
Aralaejung <aralaejung@gmail.com>
2022-06-25 15:13:54 +0700

    finish the header section

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,8 @@ import {
 
 export default function Header() {
   return (
-    <div>
-      <div className="">
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+      <div className="flex">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
         <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
@@ -20,6 +20,7 @@ export default function Header() {
         width="100"
         height="100"
         src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+        className="cursor-pointer active:brightness-110"
       />
     </div>
   );
